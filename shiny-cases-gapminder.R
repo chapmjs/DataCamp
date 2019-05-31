11e424bceea97b5bcf70e207e5fe2118862c6c48
John Chapman <684704+chapmjs@users.noreply.github.com>
2019-05-31 13:57:48 -0400

    first part of 3rd chapter of shiny: cases studies

--- shiny-cases-gapminder.R
+++ shiny-cases-gapminder.R
@@ -2,10 +2,42 @@
 # started 20190528
 
 library(gapminder)
+library(shiny)
 
-min(gapminder$pop)
-max(gapminder$pop)
+# min(gapminder$pop)
+# max(gapminder$pop)
+# 
+# subset(gapminder, country == "Canada" & year < 1965)
+# 
+# subset(gapminder, country == "Canada" & year == 1962)$lifeExp
 
-subset(gapminder, country == "Canada" & year < 1965)
+ui <- fluidPage(
+  h1("Gapminder"),
+  sliderInput(inputId = "life", label = "Life expectancy",
+              min = 0, max = 120,
+              value = c(30, 50)),
+  # Add an "All" value to the continent list
+  selectInput("continent", "Continent",
+              choices = c("All", levels(gapminder$continent))),
+  tableOutput("table")
+)
 
-subset(gapminder, country == "Canada" & year == 1962)$lifeExp
+server <- function(input, output) {
+  output$table <- renderTable({
+    data <- gapminder
+    data <- subset(
+      data,
+      lifeExp >= input$life[1] & lifeExp <= input$life[2]
+    )
+    # Don't subset the data if "All" continent are chosen
+    if (input$continent != "All") {
+      data <- subset(
+        data,
+        continent == input$continent
+      )
+    }
+    data
+  })
+}
+
+shinyApp(ui, server)
