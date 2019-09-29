2cc17b4bac25c9ed12c62242aabd38edc7212eb6
John Chapman <684704+chapmjs@users.noreply.github.com>
2019-09-28 21:03:32 -0400

    First commit

---
+++ RMySQL.R
@@ -0,0 +1,11 @@
+# practice file
+
+library(RMySQL)
+
+con <- dbConnect(RMySQL::MySQL(),
+                 dbname = "tweater",
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com",
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
+print(con)
