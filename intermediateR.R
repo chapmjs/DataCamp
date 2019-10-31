3553f6d9d864ed23ad9d7bff6081d44c3c499e05
John Chapman <684704+chapmjs@users.noreply.github.com>
2019-10-31 13:44:28 -0400

    lapply, sapply functions

--- intermediateR.R
+++ intermediateR.R
@@ -19,3 +19,30 @@ while (i <= 10) {
 args(sd)
 # function (x, na.rm = FALSE)
 # NULL
+
+
+cities <- c("New York", "Paris", "London", "Tokyo", "Rio de Janeiro", "Cape Town", "Buena Vista VA")
+
+
+first_and_last <- function(name) {
+  name <- gsub(" ", "", name)
+  letters <- strsplit(name,split="")[[1]]
+  c(first = min(letters), last = max(letters))
+}
+
+first_and_last("New York")
+first_and_last("John Shadrack Chapman")
+
+sapply(cities, first_and_last)
+
+lapply(cities, first_and_last)
+
+unique_letters <- function(name) {
+  name <- gsub(" ", "", name)
+  letters <- strsplit(name, split = "")[[1]]
+  unique(letters)
+}
+
+lapply(cities, unique_letters)
+
+sapply(cities, unique_letters)
